1c054855266ec1ff308a7d8fb74b302ef14dc46b
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-06-08 13:55:20 +0200

    feat: update 01

--- benches/01.rs
+++ benches/01.rs
@@ -1,10 +1,13 @@
 use futures::stream::FuturesUnordered;
 use std::hint::black_box;
-use tokio::{runtime::Runtime, sync::mpsc};
+use tokio::{runtime::Builder, sync::mpsc};
 
 use criterion::{Criterion, criterion_group, criterion_main};
 
-use async_rust_benchmarks::_01::{future::FutureActor, select::RandomSelectActor};
+use async_rust_benchmarks::_01::{
+    future::FutureActor,
+    select::{BiasedSelectActor, RandomSelectActor},
+};
 
 async fn run_future_actor(num_tasks: u64) {
     let (task_sender, task_receiver) = mpsc::channel(num_tasks as usize);
@@ -48,15 +51,47 @@ async fn run_random_select_actor(num_tasks: u64) {
     tokio::spawn(actor.run());
 
     // Send tasks
-    for i in 1..=num_tasks {
-        task_sender.send(i).await.unwrap();
-    }
+    tokio::spawn(async move {
+        for i in 1..=num_tasks {
+            task_sender.send(i).await.unwrap();
+        }
+    });
 
     // Collect results
-    let mut results = Vec::new();
-    while results.len() < num_tasks as usize {
-        if let Some(result) = result_receiver.recv().await {
-            results.push(result);
+    let mut i = 0;
+    while i < num_tasks as usize {
+        if result_receiver.recv().await.is_some() {
+            i += 1;
+        } else {
+            break;
+        }
+    }
+}
+
+async fn run_biased_select_actor(num_tasks: u64) {
+    let (task_sender, task_receiver) = mpsc::channel(num_tasks as usize);
+    let (result_sender, mut result_receiver) = mpsc::channel(num_tasks as usize);
+
+    let actor = BiasedSelectActor {
+        incoming_tasks: task_receiver,
+        processing_tasks: FuturesUnordered::new(),
+        results: result_sender,
+    };
+
+    tokio::spawn(actor.run());
+
+    // Send tasks
+    tokio::spawn(async move {
+        for i in 1..=num_tasks {
+            task_sender.send(i).await.unwrap();
+        }
+    });
+
+    // Collect results
+    let mut i = 0;
+    while i < num_tasks as usize {
+        if result_receiver.recv().await.is_some() {
+            i += 1;
         } else {
             break;
         }
@@ -64,16 +99,42 @@ async fn run_random_select_actor(num_tasks: u64) {
 }
 
 fn bench_future_actor(c: &mut Criterion) {
-    // Create the Tokio runtime
-    let rt = Runtime::new().unwrap();
+    // Tokio runtime with current thread
+    let rt = Builder::new_current_thread().enable_time().build().unwrap();
 
-    c.bench_function("future_actor_10000_tasks", |b| {
-        b.to_async(&rt).iter(|| run_future_actor(black_box(10000)))
+    c.bench_function("future_actor_50000_tasks_current_thread", |b| {
+        b.to_async(&rt).iter(|| run_future_actor(black_box(50000)))
     });
 
-    c.bench_function("random_select_actor_10000_tasks", |b| {
+    c.bench_function("random_select_actor_50000_tasks_current_thread", |b| {
         b.to_async(&rt)
-            .iter(|| run_random_select_actor(black_box(10000)))
+            .iter(|| run_random_select_actor(black_box(50000)))
+    });
+
+    c.bench_function("biased_select_actor_50000_tasks_current_thread", |b| {
+        b.to_async(&rt)
+            .iter(|| run_biased_select_actor(black_box(50000)))
+    });
+
+    // Tokio runtime with multi thread (4 threads)
+    let rt = Builder::new_multi_thread()
+        .worker_threads(4)
+        .enable_time()
+        .build()
+        .unwrap();
+
+    c.bench_function("future_actor_50000_tasks_multi_thread", |b| {
+        b.to_async(&rt).iter(|| run_future_actor(black_box(50000)))
+    });
+
+    c.bench_function("random_select_actor_50000_tasks_multi_thread", |b| {
+        b.to_async(&rt)
+            .iter(|| run_random_select_actor(black_box(50000)))
+    });
+
+    c.bench_function("biased_select_actor_50000_tasks_multi_thread", |b| {
+        b.to_async(&rt)
+            .iter(|| run_biased_select_actor(black_box(50000)))
     });
 }
 
--- src/_01/select.rs
+++ src/_01/select.rs
@@ -26,8 +26,44 @@ impl RandomSelectActor {
                         }
                     }
                 }
+
                 Some(result) = self.processing_tasks.next() => {
-                    self.results.send(result).await.unwrap();
+                    self.results.try_send(result).unwrap();
+                }
+            }
+        }
+    }
+}
+
+pub struct BiasedSelectActor {
+    pub incoming_tasks: mpsc::Receiver<u64>,
+    pub processing_tasks: FuturesUnordered<Pin<Box<dyn Future<Output = u64> + Send>>>,
+    pub results: mpsc::Sender<u64>,
+}
+
+impl BiasedSelectActor {
+    pub async fn run(mut self) {
+        loop {
+            tokio::select! {
+                // Interestingly, biasing in this specific order is faster than anything else.
+                biased;
+
+                task = self.incoming_tasks.recv() => {
+                    match task {
+                        Some(task) => {
+                            self.processing_tasks.push(Box::pin(async move {
+                                tokio::time::sleep(Duration::from_micros(10)).await;
+                                task * 2
+                            }));
+                        }
+                        None => {
+                            return;
+                        }
+                    }
+                }
+
+                Some(result) = self.processing_tasks.next() => {
+                    self.results.try_send(result).unwrap();
                 }
             }
         }
